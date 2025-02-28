500e8ab20dd580ea320c1ec82959ad04509727cd
seuthootDev <seuthootdev@gmail.com>
2025-02-28 22:37:50 +0900

    deploy6

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 
 
 - https://lucide.dev/icons/
+
+
+# vercel 관련
+ - pages/api/hello.js 파일 생성
+ - package.json 에 axios 의존성 추가
+ - next.config.mjs 에 코드 추가가
--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,9 @@ const nextConfig = {
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+          { key: 'Access-Control-Allow-Headers', value: '*' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Max-Age', value: '86400' }
         ],
       },
     ]
