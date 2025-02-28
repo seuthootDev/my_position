2e64f415f17ba8957c2175a26b1c85993f6701ae
seuthootDev <seuthootdev@gmail.com>
2025-02-28 22:39:22 +0900

    deploy7

--- pages/api/run-api.js
+++ pages/api/run-api.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+  // OPTIONS 요청 처리
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
